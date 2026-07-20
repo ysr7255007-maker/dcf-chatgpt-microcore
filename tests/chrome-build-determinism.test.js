e5c51bf7deb61a77d78a638153e1f52a48ea1e87
ysr7255007-maker <ysr7255007@gmail.com>
2026-07-20 22:27:18 +0800

    test: track rc.2.2 deterministic artifact

--- tests/chrome-build-determinism.test.js
+++ tests/chrome-build-determinism.test.js
@@ -7,7 +7,7 @@ const childProcess = require('child_process');
 const root = path.resolve(__dirname, '..');
 function buildHash() {
   childProcess.execFileSync(process.execPath, ['scripts/build-chrome-extension.js'], { cwd: root, stdio: 'ignore' });
-  return crypto.createHash('sha256').update(fs.readFileSync(path.join(root, 'dist/dcf-chrome-extension-1.0.0-rc.2.1.zip'))).digest('hex');
+  return crypto.createHash('sha256').update(fs.readFileSync(path.join(root, 'dist/dcf-chrome-extension-1.0.0-rc.2.2.zip'))).digest('hex');
 }
 assert.strictEqual(buildHash(), buildHash());
 console.log(JSON.stringify({ ok: true, deterministic_zip: true }, null, 2));
